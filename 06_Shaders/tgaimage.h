16ee4aed3a1d834d8068e135ef4b235fbdc19c8c
douysu <yindou97@163.com>
2020-04-01 16:46:56 +0800

    [com] Gouraud着色

--- 06_Shaders/tgaimage.h
+++ 06_Shaders/tgaimage.h
@@ -27,10 +27,10 @@ struct TGAColor {
     unsigned char bytespp;
 
     TGAColor() : bgra(), bytespp(1) {
-        for (int i=0; i<4; i++) bgra[i] = 0;
+        for (int i = 0; i < 4; i++) bgra[i] = 0;
     }
 
-    TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A=255) : bgra(), bytespp(4) {
+    TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A = 255) : bgra(), bytespp(4) {
         bgra[0] = B;
         bgra[1] = G;
         bgra[2] = R;
@@ -38,24 +38,26 @@ struct TGAColor {
     }
 
     TGAColor(unsigned char v) : bgra(), bytespp(1) {
-        for (int i=0; i<4; i++) bgra[i] = 0;
+        for (int i = 0; i < 4; i++) bgra[i] = 0;
         bgra[0] = v;
     }
 
 
-    TGAColor(const unsigned char *p, unsigned char bpp) : bgra(), bytespp(bpp) {
-        for (int i=0; i<(int)bpp; i++) {
+    TGAColor(const unsigned char* p, unsigned char bpp) : bgra(), bytespp(bpp) {
+        for (int i = 0; i < (int)bpp; i++) {
             bgra[i] = p[i];
         }
-        for (int i=bpp; i<4; i++) {
+        for (int i = bpp; i < 4; i++) {
             bgra[i] = 0;
         }
     }
 
+    unsigned char& operator[](const int i) { return bgra[i]; }
+
     TGAColor operator *(float intensity) const {
         TGAColor res = *this;
-        intensity = (intensity>1.f?1.f:(intensity<0.f?0.f:intensity));
-        for (int i=0; i<4; i++) res.bgra[i] = bgra[i]*intensity;
+        intensity = (intensity > 1.f ? 1.f : (intensity < 0.f ? 0.f : intensity));
+        for (int i = 0; i < 4; i++) res.bgra[i] = bgra[i] * intensity;
         return res;
     }
 };
@@ -68,30 +70,30 @@ protected:
     int height;
     int bytespp;
 
-    bool   load_rle_data(std::ifstream &in);
-    bool unload_rle_data(std::ofstream &out);
+    bool   load_rle_data(std::ifstream& in);
+    bool unload_rle_data(std::ofstream& out);
 public:
     enum Format {
-        GRAYSCALE=1, RGB=3, RGBA=4
+        GRAYSCALE = 1, RGB = 3, RGBA = 4
     };
 
     TGAImage();
     TGAImage(int w, int h, int bpp);
-    TGAImage(const TGAImage &img);
-    bool read_tga_file(const char *filename);
-    bool write_tga_file(const char *filename, bool rle=true);
+    TGAImage(const TGAImage& img);
+    bool read_tga_file(const char* filename);
+    bool write_tga_file(const char* filename, bool rle = true);
     bool flip_horizontally();
     bool flip_vertically();
     bool scale(int w, int h);
     TGAColor get(int x, int y);
-    bool set(int x, int y, TGAColor &c);
-    bool set(int x, int y, const TGAColor &c);
+    bool set(int x, int y, TGAColor& c);
+    bool set(int x, int y, const TGAColor& c);
     ~TGAImage();
-    TGAImage & operator =(const TGAImage &img);
+    TGAImage& operator =(const TGAImage& img);
     int get_width();
     int get_height();
     int get_bytespp();
-    unsigned char *buffer();
+    unsigned char* buffer();
     void clear();
 };
 
